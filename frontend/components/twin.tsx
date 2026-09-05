8d097d26b439ff7909269a6278cf523ff247b8c4
Peter Strössler <7starch@gmail.com>
2026-09-05 14:21:55 +0200

    corrected avatar jpg name

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -142,7 +142,7 @@ export default function Twin() {
                             <div className="flex-shrink-0">
                                 {hasAvatar ? (
                                     <img 
-                                        src="jpg" 
+                                        src="avatar.jpg" 
                                         alt="Digital Twin Avatar" 
                                         className="w-8 h-8 rounded-full border border-slate-300"
                                     />
